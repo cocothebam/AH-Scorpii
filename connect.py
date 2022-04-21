99798cbaea6d794066254d061187dcba9ba6c399
Jack Jewett <JackJewett818@gmail.com>
2022-04-21 18:04:36 -0400

    minor improvements

--- connect.py
+++ connect.py
@@ -8,17 +8,16 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 
 bot = commands.Bot(command_prefix="-")
 
+
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
     CommandsBot()
-
 
 
 bot.run(TOKEN)
